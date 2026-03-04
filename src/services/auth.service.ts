0353971d0b377793104276f6ef69edbf08bb77a2
Mustafa Çaylı <caylimustafa12@gmail.com>
2026-03-04 04:21:56 +0300

    auth service test 2

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -24,23 +24,28 @@ export class AuthService {
 
     // 2. Kullanıcı Girişi (Login)
     static async login(email: string, password: string): Promise<{ user: User, token: string }> {
-        const user = await UserRepository.findByEmail(email);
-        if (!user) {
-            throw new Error('Kullanıcı bulunamadı.');
-        }
-        console.log("DB'den gelen şifre:", user.password);
-        console.log("Girilen şifre:", password);    
-        // DÜZELTME: user.password_hash yerine user.password kullanıyoruz
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            throw new Error('Hatalı şifre.');
-        }
-        console.log("Eşleşme sonucu:", isPasswordValid);
+    console.log("--- LOGIN DENEMESİ BAŞLADI ---");
+    console.log("1. Frontend'den Gelen Email:", email);
+    console.log("2. Frontend'den Gelen Şifre Uzunluğu:", password?.length);
 
-        const jwtSecret = process.env.JWT_SECRET || 'gizli_anahtar_yedek';
-        const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '24h' });
-        
-
-        return { user, token };
+    const user = await UserRepository.findByEmail(email);
+    
+    if (!user) {
+        console.log("HATA: Bu email ile kullanıcı bulunamadı.");
+        throw new Error('Kullanıcı bulunamadı.');
     }
+
+    console.log("3. DB'den Bulunan Kullanıcı:", user.email);
+    console.log("4. DB'deki Şifre Hash'i:", user.password);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log("5. Şifre Eşleşme Sonucu:", isPasswordValid);
+
+    if (!isPasswordValid) throw new Error('Hatalı şifre.');
+
+    const jwtSecret = process.env.JWT_SECRET || 'gizli_anahtar_yedek';
+    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '24h' });
+
+    return { user, token };
+}
 }
